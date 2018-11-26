9217b5fe90b11aaef76a0ecf9ccfd7f703166544
ezzabuzaid <eabuzaid@noventapp.com>
2018-11-26 13:24:48 +0200

    copy html, compile ts,  minify js done

--- gulpfile.js
+++ gulpfile.js
@@ -1,66 +1,99 @@
-const {
-    dest,
-    series,
-    parallel,
-    src,
-    task,
-    watch
-} = require("gulp");
+const gulp = require("gulp");
+
 const browserify = require("browserify");
-const source = require('vinyl-source-stream');
 const tsify = require("tsify");
+var fs = require('fs');
+
+const source = require('vinyl-source-stream');
 const buffer = require('vinyl-buffer');
-const uglify = require('gulp-uglify');
-const sourcemaps = require('gulp-sourcemaps');
-const minifyInline = require('gulp-minify-inline');
-const clean = require('gulp-clean');
-const htmlmin = require('gulp-htmlmin');
-const minify = require('gulp-minify');
+const uglify = require('gulp-uglify-es').default;
 const del = require('del');
+const gutil = require('gulp-util');
+const minifyJS = require('gulp-minify');
+const htmlmin = require('gulp-htmlmin');
 const paths = {
-    pages: ['src/*.html'],
-    scripts: ['src/*.js']
+    pages: ['dist/index.html'],
+    scripts: ['dist/**/*.js']
 };
-const TASK_LIST = ["clean", "minify-html", "compress"];
-task('minify-html', () => {
-    return src(paths.pages)
-        .pipe(htmlmin({
-            collapseWhitespace: true,
-            removeComments: true
-        }))
-        // .pipe(minifyInline())
-        .pipe(dest('dist'));
+const TASK_LIST = ['clean', 'compile-ts', ['minify-compress', 'copy-html'], 'minify-html'];
+
+const gulpCopy = require('gulp-copy');
+
+gulp.task('clean', () => del(['dist']));
+gulp.task('minify-html', () => {
+    return gulp.src(paths.pages)
+        .pipe(htmlmin({ collapseWhitespace: true, useShortDoctype: true, removeStyleLinkTypeAttributes: true, removeComments: true }))
+        .pipe(gulp.dest('dist'));
 });
-task('clean', function (cb) {
-    del(['dist/*'], cb)
+gulp.task('copy-html', () => {
+    return gulp.src('./src/index.html')
+        .pipe(gulpCopy('dist/', { prefix: 1 }))
+        .pipe(gulp.dest('dist'));
 });
-task('compress', function () {
-    src(paths.scripts)
-        .pipe(minify())
-        .pipe(dest('dist'))
+gulp.task('minify-compress', () => {
+    return gulp.src(paths.scripts)
+        .pipe(minifyJS())
+        .pipe(uglify())
+        .pipe(gulp.dest('dist'))
 });
-task("default", [...TASK_LIST, "watch"], function () {
+gulp.task('compile-ts', () => {
     return browserify({
-            basedir: '.',
-            debug: true,
-            entries: ['src/main.ts'],
-            cache: {},
-            packageCache: {}
-        })
+        basedir: '.',
+        debug: false,
+        entries: ['src/main.ts'],
+        cache: {},
+        packageCache: {},
+    })
+        // .pipe(watch('src/**/*'))
         .plugin(tsify)
         .bundle()
+        .on('error', function (err) { gutil.log(gutil.colors.red('[Error]'), err.toString()); })
         .pipe(source('bundle.js'))
-        .pipe(buffer())
-        .pipe(uglify())
-        .pipe(dest("dist"));
-});
-task('watch', function () {
-    watch('src/**/*.ts', TASK_LIST);
-    watch('src/**/*.css', TASK_LIST);
-    watch('src/**/*.html', TASK_LIST);
-});
+        .pipe(gulp.dest("dist"));
+})
+const sugar = (...a) => gulp.series(...a.map((i) => Array.isArray(i) ? gulp.parallel(...i) : i));
+
+gulp.task("default", sugar(...TASK_LIST));
+// task('watch', function () {
+//     watch('src/**/*.ts', TASK_LIST);
+//     watch('src/**/*.css', TASK_LIST);
+//     watch('src/**/*.html', TASK_LIST);
+// });
 // task('stream', function () {
 //     return watch('src/**/*.ts', {
 //         ignoreInitial: false
 //     });
 // });
+
+// {del} :: https://github.com/gulpjs/gulp/blob/master/docs/recipes/delete-files-folder.md
+
+const AUTOPREFIXER_BROWSERS = [
+    'ie >= 10',
+    'ie_mob >= 10',
+    'ff >= 30',
+    'chrome >= 34',
+    'safari >= 7',
+    'opera >= 23',
+    'ios >= 7',
+    'android >= 4.4',
+    'bb >= 10'
+];
+
+
+// Gulp task to minify CSS files
+gulp.task('styles', function () {
+    return gulp.src('./src/sass/styles.scss')
+        // Compile SASS files
+        .pipe(sass({
+            outputStyle: 'nested',
+            precision: 10,
+            includePaths: ['.'],
+            onError: console.error.bind(console, 'Sass error:')
+        }))
+        // Auto-prefix css styles for cross browser compatibility
+        .pipe(autoprefixer({ browsers: AUTOPREFIXER_BROWSERS }))
+        // Minify the file
+        .pipe(csso())
+        // Output
+        .pipe(gulp.dest('./dist/css'))
+});
